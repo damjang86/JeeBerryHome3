064568c41155b211cf69612ce6a0469147b66dfc
Pavle Đorđević <paja.92@gmail.com>
2015-02-08 21:41:48 +0100

    GitIgnore updated, added controllers

---
+++ Controllers/BarsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JeeBerryHome3.Controllers
+{
+    public class BarsController : Controller
+    {
+        //
+        // GET: /Bars/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Neo4jClient.Cypher;
-using Neo4jClient;
-using JeeBerryHome3.Models;
 
 namespace JeeBerryHome3.Controllers
 {
     public class HomeController : Controller
     {
-        public GraphClient client;
         public ActionResult Index()
         {
             ViewBag.Message = "";
@@ -29,6 +25,7 @@ namespace JeeBerryHome3.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
             return View();
         }
 
@@ -50,20 +47,6 @@ namespace JeeBerryHome3.Controllers
         public ActionResult Messages()
         {
             ViewBag.Message = "Your contact page.";
-
-            client = new GraphClient(new Uri("http://localhost:7474/db/data"));
-            client.Connect();
-
-            Band band = new Band();
-            band.Name = "Amadeus";
-            Dictionary<string, object> queryDict = new Dictionary<string, object>();
-            queryDict.Add("name", band.Name);
-
-            var query = new Neo4jClient.Cypher.CypherQuery("CREATE (NewBand:Band {name:'" + band.Name + "'}) return NewBand",
-                                                            queryDict, CypherResultMode.Set);
-
-            List<Band> actors = ((IRawGraphClient)client).ExecuteGetCypherResults<Band>(query).ToList();
-
             return View();
 
             
