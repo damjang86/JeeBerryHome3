1df1cce36e9ece852538bf372785bb5ed8fb8692
Pavle Đorđević <paja.92@gmail.com>
2015-02-08 20:54:52 +0100

    Dodat Neo4j
    Testirana Neo4j funkcionalnost, prilikom odlaska na tab poruke, pravi se
    test Node u bazi.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Neo4jClient.Cypher;
+using Neo4jClient;
+using JeeBerryHome3.Models;
 
 namespace JeeBerryHome3.Controllers
 {
     public class HomeController : Controller
     {
+        public GraphClient client;
         public ActionResult Index()
         {
             ViewBag.Message = "";
@@ -25,7 +29,6 @@ namespace JeeBerryHome3.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
@@ -47,6 +50,20 @@ namespace JeeBerryHome3.Controllers
         public ActionResult Messages()
         {
             ViewBag.Message = "Your contact page.";
+
+            client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+            client.Connect();
+
+            Band band = new Band();
+            band.Name = "Amadeus";
+            Dictionary<string, object> queryDict = new Dictionary<string, object>();
+            queryDict.Add("name", band.Name);
+
+            var query = new Neo4jClient.Cypher.CypherQuery("CREATE (NewBand:Band {name:'" + band.Name + "'}) return NewBand",
+                                                            queryDict, CypherResultMode.Set);
+
+            List<Band> actors = ((IRawGraphClient)client).ExecuteGetCypherResults<Band>(query).ToList();
+
             return View();
 
             
